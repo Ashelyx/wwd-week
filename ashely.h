31d33b5b3d35b44457ef532a7e7f7b1f750f50ce
Ashelyx <ashelycodinater@gmail.com>
2025-09-10 04:48:41 -0700

    Add files via upload

---
+++ ashely.h
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en-US">
+   <heade>
+         <Meta charest="UTF-8">
+         <meta name="viewport" content="width=device-width intial-scale=1.0">
+         <tittle>jasoon blazzard | WWD 130</tittle>
+    </head>
+    <body>
+      <header>
+      <nav>
+        <a href="#">home</a>
+        <a href="wwr/"> Rafting website</a>
+    </nav>
+</header>
+<main>  
+    <h1> your jason Blazzer | WDD 130</h1> 
+    <img>
+      src="images/profile.jpg"
+      Ait="jason brazzer"
+      width="200"
+        Hello! I am Your Ashely okaka, and I am excited to start learning web development in this course. 
+        I enjoy [your hobbies or interests]. I look forward to creating amazing websites!
+      </p>    
+    </main>
+    <footer>
+      <p>©2025 🌟 "Ashely okaka" 🌟 kampala, uganda</p>
+    </footer>
+  </body>
+
+</html>
+
+
+
+
